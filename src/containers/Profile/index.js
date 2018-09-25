40340031c7a284007508e6266f6e5acc803c7b31
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-09-25 23:04:14 +0900

    update: font color of tag buttons

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -368,7 +368,6 @@ class Users extends Component {
         "margin-right": "10px",
         "margin-bottom": "10px",
         "background-color": "#04B486",
-        "color": "white",
         "text-transform": "none",
       },
       addstyle: {
@@ -437,13 +436,11 @@ class Users extends Component {
          <div style={style.tagstyle}>
            {this.state.tags.map((tag,i)=>{
              return (
-               <Button key={i} variant="contained" color="primary" style={style.tagbtnstyle}>
-               <EditableLabel value={[tag]}
-                              onEditEnd={(val)=>this.onParamsEditEnd(tag,val,this.modalMODES.tag)}
-                              onClick={()=>this.toTagPage(tag)}
-                              canEdit={true}
-               />
-               </Button>
+              <Button key={i} variant="contained" style={style.tagbtnstyle}>
+                <span style={{color: "white"}}>{[tag]}</span>
+                &nbsp;&nbsp;
+                <span style={{color: "block"}}>X</span>
+              </Button>
              );
            })}
            <Button mini
