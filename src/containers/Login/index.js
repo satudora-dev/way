760972a9ef0c4102234c38224fb6e786feb0821b
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-01 08:42:34 +0900

    reduce padding-top value

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -75,10 +75,9 @@ class Login extends Component {
   render() {
     const style = {
       container: {
-        "padding-top": "200px",
+        "padding-top": "80px",
         "padding-bottom": "100px",
         width: "100%",
-        //"line-height": "300px",
         "text-align": "center",
         position: "relative",
       }
