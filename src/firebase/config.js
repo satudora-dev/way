c179d11ecf7645d68a56b8efa87274c952c67262
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-10-10 13:15:52 +0900

    update: auth domain to "way.satudora.co"

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAPqTg9vMyEenl5t7XR9pdakgaUqa4WdOY",
-  authDomain: "whoareyou-c231f.firebaseapp.com",
+  authDomain: "way.satudora.co",
   databaseURL: "https://whoareyou-c231f.firebaseio.com",
   projectId: "whoareyou-c231f",
   storageBucket: "whoareyou-c231f.appspot.com",
