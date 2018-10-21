b3a839216bf8276d6e9982defa21cda0a477bacc
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-21 17:06:28 +0900

    createstore with thunk

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import Routes from './routes';
 import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import rootReducer from '../reducers';
+import reduxThunk from "redux-thunk";
+
+
+const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+
+
+ReactDOM.render(<Routes store={store}/>, document.getElementById('root'));
 registerServiceWorker();
