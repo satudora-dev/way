583dab64f7f388948cefc2f8666dca79012675ef
TetsuFe <tetsubeg1ner27@gmail.com>
2018-10-21 20:11:31 +0900

    modify firebase config can switch in response to 3 development

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,32 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAPqTg9vMyEenl5t7XR9pdakgaUqa4WdOY",
-  authDomain: "whoareyou-c231f.firebaseapp.com",
-  databaseURL: "https://whoareyou-c231f.firebaseio.com",
-  projectId: "whoareyou-c231f",
-  storageBucket: "whoareyou-c231f.appspot.com",
-  messagingSenderId: "43340297718"
-};
+export const firebaseConfig = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return {
+      apiKey: "AIzaSyCBXWCzAv-oAT-HlUgNdHHIgh7fl2O5uDE",
+      authDomain: "whoareyou-dev.firebaseapp.com",
+      databaseURL: "https://whoareyou-dev.firebaseio.com",
+      projectId: "whoareyou-dev",
+      storageBucket: "whoareyou-dev.appspot.com",
+      messagingSenderId: "944348152216"
+    };
+  } else if (process.env.NODE_ENV === 'test') {
+    // developmentととりあえず同じ
+    return {
+      apiKey: "AIzaSyCBXWCzAv-oAT-HlUgNdHHIgh7fl2O5uDE",
+      authDomain: "whoareyou-dev.firebaseapp.com",
+      databaseURL: "https://whoareyou-dev.firebaseio.com",
+      projectId: "whoareyou-dev",
+      storageBucket: "whoareyou-dev.appspot.com",
+      messagingSenderId: "944348152216"
+    };
+  } else if (process.env.NODE_ENV === 'production') {
+    return {
+      apiKey: "AIzaSyAPqTg9vMyEenl5t7XR9pdakgaUqa4WdOY",
+      authDomain: "whoareyou-c231f.firebaseapp.com",
+      databaseURL: "https://whoareyou-c231f.firebaseio.com",
+      projectId: "whoareyou-c231f",
+      storageBucket: "whoareyou-c231f.appspot.com",
+      messagingSenderId: "43340297718"
+    };
+  }
+  return {}
+}
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import {firebaseConfig} from './config.js';
 
-export const firebaseApp=firebase.initializeApp(firebaseConfig);
+export const firebaseApp=firebase.initializeApp(firebaseConfig());
 export const firebaseDB=firebaseApp.database();
 export const firebaseAuth=firebase.auth;
 export const firebaseStorage=firebase.storage();
