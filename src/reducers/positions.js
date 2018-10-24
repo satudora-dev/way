d970d429a5cff5f7dbd9a2822a1c5dc0c372b0f3
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-24 18:38:39 +0900

    tweak position state structure

--- src/reducers/positions.js
+++ src/reducers/positions.js
@@ -1,20 +1,19 @@
 const positions = (state = [], action) => {
   switch (action.type) {
     case 'POSITIONS_RECEIVE_DATA':
-      let positions = []
+      let positions = {}
       if(action.data){
         Object.keys(action.data).forEach(key =>{
           let userids = []
           Object.keys(action.data[key]).forEach(userid =>{
             userids.push({userid})
           });
-          positions.push({
-            position: key,
+          positions[key]={
             userids: userids,
-          })
+          }
         });
       }
-      return [...positions]
+      return positions
     case 'POSITIONS_RECEIVE_ERROR':
     case 'ADD_POSITION_ERROR':
       alert(action.message)
