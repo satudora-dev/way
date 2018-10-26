932cc54264fd26bcbd1421823ce3eb1bc910fc52
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-26 16:54:01 +0900

    Fix missed variable name

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,7 @@
 const auth = (state = {init: true}, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
-      return {ownkey: action.ownkey}
+      return {ownKey: action.ownKey}
     default:
       return state
   }
