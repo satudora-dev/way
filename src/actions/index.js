03f7c077698a43979fbcac0a10d24c6b0fa8ebbf
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-26 13:14:51 +0900

    Define initFetchIfLoggedIn action

---
+++ src/actions/index.js
@@ -0,0 +1,122 @@
+import { firebaseDB, firebaseStorage, firebaseAuth  } from '../firebase';
+
+const accountRef = firebaseDB.Ref('accounts');
+const positionRef = firebaseDB.Ref('positions');
+const projectRef = firebaseDB.Ref('projects');
+const tagRef = firebaseDB.Ref('tags');
+const userRef = firebaseDB.Ref('users');
+
+const fetchAccountsError = error => {
+  return {
+    type: 'ACCOUNTS_RECEIVE_ERROR',
+    message: error.message
+  }
+}
+
+
+
+const fetchPositionsSuccess = snapshot => {
+  return {
+    type: 'POSITIONS_RECEIVE_DATA',
+    data: snapshot.val()
+  }
+}
+
+const fetchPositionsError = error => {
+  return {
+    type: 'POSITIONS_RECEIVE_ERROR',
+    message: error.message
+  }
+}
+
+
+const fetchProjectsSuccess = snapshot => {
+  return {
+    type: 'PROJECTS_RECEIVE_DATA',
+    data: snapshot.val()
+  }
+}
+
+const fetchProjectsError = error => {
+  return {
+    type: 'PROJECTS_RECEIVE_ERROR',
+    message: error.message
+  }
+}
+
+
+
+const fetchTagsSuccess = snapshot => {
+  return {
+    type: 'TAGS_RECEIVE_DATA',
+    data: snapshot.val()
+  }
+}
+
+const fetchTagsError = error => {
+  return {
+    type: 'TAGS_RECEIVE_ERROR',
+    message: error.message
+  }
+}
+
+
+
+const fetchUsersSuccess = snapshot => {
+  return {
+    type: 'USERS_RECEIVE_DATA',
+    data: snapshot.val()
+  }
+}
+
+const fetchUsersError = error => {
+  return {
+    type: 'USERS_RECEIVE_ERROR',
+    message: error.message
+  }
+}
+
+const setCurrentUser = ownKey  => {
+  return {
+    type: 'SET_CURRENT_USER',
+    ownKey: ownKey
+  }
+}
+
+export const initFetchIfLoggedIn = () => dispatch => {
+  firebaseAuth().onAuthStateChanged(user=>{
+    if(user){
+      accountRef.off()
+      accountRef.on('value',
+        (snapshot) => {dispatch(fetchAccountsSuccess(snapshot))},
+        (error) => {dispatch(fetchAccountsError(error))}
+      )
+      positionRef.off()
+      positionRef.on('value',
+        (snapshot) => {dispatch(fetchPositionsSuccess(snapshot))},
+        (error) => {dispatch(fetchPositionsError(error))}
+      )
+      projectRef.off()
+      projectRef.on('value',
+        (snapshot) => {dispatch(fetchProjectsSuccess(snapshot))},
+        (error) => {dispatch(fetchProjectsError(error))}
+      )
+      tagRef.off()
+      tagRef.on('value',
+        (snapshot) => {dispatch(fetchTagsSuccess(snapshot))},
+        (error) => {dispatch(fetchTagsError(error))}
+      )
+      userRef.off()
+      userRef.on('value',
+        (snapshot) => {dispatch(fetchUsersSuccess(snapshot))},
+        (error) => {dispatch(fetchUsersError(error))}
+      )
+      accountRef.orderByChild('email').equalTo(user.email).once('value', (snapshot) =>{
+        let ownKey = Object.keys(snapshot.val())[0];
+        dispatch(setCurrentUser(ownKey))
+      })
+    }else{
+      dispatch(setCurrentUser(null))
+    }
+  })
+}
