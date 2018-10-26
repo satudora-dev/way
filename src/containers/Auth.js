f46747405b821c53fb11e7a98774f2d30da6072d
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-26 16:43:32 +0900

    Create Auth Component to execute initFetchIfLoggedIn

---
+++ src/containers/Auth.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+class Auth extends React.Component {
+
+  componentWillMount(){
+    this.props.initFetchIfLoggedIn()
+  }
+  render() {
+    return null;
+  }
+}
+
+const mapStateToProps = ( state ) => {
+  const ownkey = state.auth.ownkey;
+  return {
+    ownkey: ownkey,
+    hasOwnProfile: state.users[ownkey] !== undefined,
+  }
+}
+
+
+const mapDispatchToProps = {
+  initFetchIfLoggedIn: actions.initFetchIfLoggedIn,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Auth);
