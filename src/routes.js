9cb442761e2dad128a6ec8ff1ca5a40694c2760d
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-26 14:03:13 +0900

    Let Routes receive and provide store

--- src/routes.js
+++ src/routes.js
@@ -8,18 +8,22 @@ import User from './containers/User';
 import Profile from './containers/Profile';
 import Logout from './containers/Logout';
 
-const Routes = () => {
+import { Provider } from 'react-redux';
+
+const Routes = ({ store }) => {
   return(
-    <BrowserRouter >
-      <div>
-        <Route exact path="/" component={ App } />
-        <Route path="/signup" component={ Signup } />
-        <Route exact path="/users" component={ User } />
-        <Route path="/users/:id" component={ Profile }/>
-        <Route path="/login" component={ Login } />
-        <Route path="/logout" component={ Logout } />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter >
+        <div>
+          <Route exact path="/" component={ App } />
+          <Route path="/signup" component={ Signup } />
+          <Route exact path="/users" component={ User } />
+          <Route path="/users/:id" component={ Profile }/>
+          <Route path="/login" component={ Login } />
+          <Route path="/logout" component={ Logout } />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
