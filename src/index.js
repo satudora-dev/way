b422083367d0cc3f4e465d947401337490b8270e
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-26 14:02:12 +0900

    Define store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import Routes from './routes';
 import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from './reducers';
+import reduxThunk from "redux-thunk";
+
+const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+
+
+ReactDOM.render(<Routes store={store}/>, document.getElementById('root'));
 registerServiceWorker();
