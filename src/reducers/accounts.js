e5279156c566726e9d4fa3ec8c2741c14462b71a
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-27 09:49:34 +0900

    Remove unused operation

--- src/reducers/accounts.js
+++ src/reducers/accounts.js
@@ -1,18 +1,8 @@
 const accounts = (state = {init: true}, action) => {
   switch (action.type) {
     case 'ACCOUNTS_RECEIVE_DATA':
-      let accounts = {}
-      if(action.data){
-        Object.keys(action.data).forEach(key =>{
-          let account = action.data[key];
-          accounts[key] = {
-            email: account.email,
-            registered: account.registered,
-            token: account.token,
-          }
-        });
-      }
-      return accounts
+      if(action.data)
+      return action.data
     case 'ACCOUNTS_RECEIVE_ERROR':
     case 'LOGIN_ERROR':
     case 'SIGNOUT_ERROR':
