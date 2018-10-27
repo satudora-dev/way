b70ca4d128919aa4210dfaf8dadd317a8fe85529
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-27 10:04:46 +0900

    Remove unused Firebase API in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import {firebaseAuth} from '../../firebase';
 
 class App extends React.Component {
-  componentWillMount(){
-    firebaseAuth().onAuthStateChanged(user=>{
-      if(user){
-        this.props.history.push('/signup');
-      }
-      else{
-        this.props.history.push('/login');
-      }
-    });
-  }
 
   render() {
+    this.props.history.push('./login')
     return (
       <div className="App">
         <h2>Now Loading ...</h2>
