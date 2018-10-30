0c67307c341ab7e388849748a8ec05cb9b0da6c0
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-30 15:57:25 +0900

    Change Auth Component's role, let Auth to prevent rendering before Fetching

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 
-import Auth from './containers/Auth'
+import Auth from './containers/Auth';
 import App from './containers/App';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
@@ -14,17 +14,18 @@ import { Provider } from 'react-redux';
 const Routes = ({ store }) => {
   return(
     <Provider store={store}>
-      <BrowserRouter >
-        <div>
-          <Route path="/" component={ Auth } />
-          <Route exact path="/" component={ App } />
-          <Route path="/signup" component={ Signup } />
-          <Route exact path="/users" component={ Users } />
-          <Route path="/users/:id" component={ Profile }/>
-          <Route path="/login" component={ Login } />
-          <Route path="/logout" component={ Logout } />
-        </div>
-      </BrowserRouter>
+      <Auth>
+        <BrowserRouter >
+          <div>
+            <Route exact path="/" component={ App } />
+            <Route path="/signup" component={ Signup } />
+            <Route exact path="/users" component={ Users } />
+            <Route path="/users/:id" component={ Profile }/>
+            <Route path="/login" component={ Login } />
+            <Route path="/logout" component={ Logout } />
+          </div>
+        </BrowserRouter>
+      </Auth>
     </Provider>
   );
 }
