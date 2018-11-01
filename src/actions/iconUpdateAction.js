bd77b3d7ad1e1314b189e0c4be04534ecf7ef67a
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-01 23:19:51 +0900

    Separate updateIcon function from other actions.

--- src/actions/iconUpdateAction.js
+++ src/actions/iconUpdateAction.js
@@ -1,41 +1,45 @@
-import { firebaseStorage } from '../firebase';
+import { firebaseDB,firebaseStorage } from '../firebase';
 import EXIF from 'exif-js';
 
 const userRef = firebaseDB.ref('users');
 const storageRef = firebaseStorage.ref();
 
-export function updateIcon(icon, userKey, dispatch) {
-    if (!icon) return;
+export function updateIcon(icon, userKey) {
+    dispatch => {
+        if (!icon) return;
 
-    let key = 'icons/' + userKey;
-    storageRef.child(key).put(icon).on('state_changed', () => {
-        storageRef.child(key).getDownloadURL().then(url => {
-            userRef.child(userKey).update({
-                icon: url,
-            }).catch(error => {
-                if (error) {
-                    dispatch({
-                        type: 'UPDATE_IMAGE_ERROR',
-                        message: error.message,
+        optimizeImage(icon, dispatch, iconImage => {
+            let key = 'icons/' + userKey;
+            storageRef.child(key).put(iconImage).on('state_changed', () => {
+                storageRef.child(key).getDownloadURL().then(url => {
+                    userRef.child(userKey).update({
+                        icon: url,
+                    }).catch(error => {
+                        if (error) {
+                            dispatch({
+                                type: 'UPDATE_IMAGE_ERROR',
+                                message: error.message,
+                            });
+                        }
                     });
-                }
+                });
             });
         });
-    });
+    }
 }
 
-optimizeImage(iconFile){
+function optimizeImage(iconFile, dispatch,callback){
     let image = new Image();
-    let _this = this;
     image.onload = () => {
         let width = image.width;
         let height = image.height;
         let maxWidth = 512;
         if (width < maxWidth) {
-            return({
+            dispatch({
                 iconFile: iconFile,
                 iconSrc: URL.createObjectURL(iconFile),
             });
+            callback(iconFile);
         }
         else {
             let canvas = document.createElement('canvas');
@@ -45,7 +49,7 @@ optimizeImage(iconFile){
 
             var orientation;
 
-            EXIF.getData(iconFile, ()=> {
+            EXIF.getData(iconFile, () => {
                 orientation = iconFile.exifdata.Orientation;
 
                 var image_aspect, canvas_width, canvas_height, draw_width, draw_height;
@@ -119,12 +123,78 @@ optimizeImage(iconFile){
 
                 // 変換後の画像をステートに設定
                 let transformedImage = canvas.toDataURL('image/png');
-                _this.setState({ iconSrc: transformedImage });
-                canvas.toBlob((blob) => {
-                    _this.setState({ iconFile: blob });
+                canvas.toBlob(blob => {
+                    dispatch({
+                        iconFile: blob,
+                        iconSrc: transformedImage
+                    });
+                    callback(blob);
                 });
             });
         }
     };
     image.src = URL.createObjectURL(iconFile);
+}
+
+// iPhoneで撮った写真はブラウザ上で回転してしまう。
+// exifに応じて画像の変換(上下左右反転と回転）
+/*
+function alignImageOrient(iconFile,ctx){
+    orientation = iconFile.exifdata.Orientation;
+
+    switch (orientation) {
+        case 2:
+            ctx.transform(-1, 0, 0, 1, canvas_width, 0);
+            break;
+
+        case 3:
+            ctx.transform(-1, 0, 0, -1, canvas_width, canvas_height);
+            break;
+
+        case 4:
+            ctx.transform(1, 0, 0, -1, 0, canvas_height);
+            break;
+
+        case 5:
+            ctx.transform(-1, 0, 0, 1, 0, 0);
+            ctx.rotate((90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
+            break;
+
+        case 6:
+            ctx.transform(1, 0, 0, 1, canvas_width, 0);
+            ctx.rotate((90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
+            break;
+
+        case 7:
+            ctx.transform(-1, 0, 0, 1, canvas_width, canvas_height);
+            ctx.rotate((-90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
+            break;
+
+        case 8:
+            ctx.transform(1, 0, 0, 1, 0, canvas_height);
+            ctx.rotate((-90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
+            break;
+
+        default:
+            break;
+    }
+
+    if (orientation >= 5) {
+        ctx.rotate((-90 * Math.PI) / 180);
+        draw_width = canvas_height;
+        draw_height = canvas_width;
+    }
+}
+*/
+
+function resizeImage(){
+
 }
