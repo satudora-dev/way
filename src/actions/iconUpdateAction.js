3b58f3124f2357e4277d8aeb3b7e1015544d56cb
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-01 23:19:51 +0900

    Separate updateIcon function from other actions.

--- src/actions/iconUpdateAction.js
+++ src/actions/iconUpdateAction.js
@@ -1,43 +1,45 @@
 import { firebaseDB,firebaseStorage } from '../firebase';
 import EXIF from 'exif-js';
-import { storage } from 'firebase';
 
 const userRef = firebaseDB.ref('users');
 const storageRef = firebaseStorage.ref();
 
-export const updateIcon = (icon, userKey) =>
+export function updateIcon(icon, userKey) {
     dispatch => {
         if (!icon) return;
 
-        optimizeImage(icon, 
-            iconFile => uploadIcon(iconFile, userKey, dispatch));
-    }
-
-function uploadIcon(iconFile, userKey, dispatch) {
-    let key = 'icons/' + userKey;
-    let targetRef = storageRef.child(key);
-    targetRef.put(iconFile).then(snapshot => {
-        targetRef.getDownloadURL().then(url => {
-            userRef.child(userKey).update({//onで呼ばない
-                icon: url,
-            }).catch(error => {
-                dispatch({
-                    type: 'UPDATE_IMAGE_ERROR',
-                    message: error.message,
+        optimizeImage(icon, dispatch, iconImage => {
+            let key = 'icons/' + userKey;
+            storageRef.child(key).put(iconImage).on('state_changed', () => {
+                storageRef.child(key).getDownloadURL().then(url => {
+                    userRef.child(userKey).update({
+                        icon: url,
+                    }).catch(error => {
+                        if (error) {
+                            dispatch({
+                                type: 'UPDATE_IMAGE_ERROR',
+                                message: error.message,
+                            });
+                        }
+                    });
                 });
             });
         });
-    });
+    }
 }
 
-function optimizeImage(iconFile, uploadIcon) {
+function optimizeImage(iconFile, dispatch,callback){
     let image = new Image();
     image.onload = () => {
-        let width = image.naturalWidth;
-        let height = image.naturalHeight;
+        let width = image.width;
+        let height = image.height;
         let maxWidth = 512;
         if (width < maxWidth) {
-            uploadIcon(iconFile);
+            dispatch({
+                iconFile: iconFile,
+                iconSrc: URL.createObjectURL(iconFile),
+            });
+            callback(iconFile);
         }
         else {
             let canvas = document.createElement('canvas');
@@ -63,10 +65,7 @@ function optimizeImage(iconFile, uploadIcon) {
                 const dst_height = canvas_height * scale;
                 canvas.width = dst_width;
                 canvas.height = dst_height;
-                ctx.scale(scale, scale);
-                
-                //image.src = resizeImage(image, maxWidth);
-                console.log(image);
+                ctx.scale(scale, scale)
 
                 // iPhoneで撮った写真はブラウザ上で回転してしまう。
                 // exifに応じて画像の変換(上下左右反転と回転）
@@ -99,6 +98,7 @@ function optimizeImage(iconFile, uploadIcon) {
                         ctx.rotate((90 * Math.PI) / 180);
                         draw_width = canvas_height;
                         draw_height = canvas_width;
+                        console.log("6!")
                         break;
 
                     case 7:
@@ -119,11 +119,17 @@ function optimizeImage(iconFile, uploadIcon) {
                         break;
                 }
 
-                ctx.drawImage(image, 0, 0, draw_width, draw_height);
-                
+                ctx.drawImage(image, 0, 0, draw_width, draw_height)
+
+                // 変換後の画像をステートに設定
+                let transformedImage = canvas.toDataURL('image/png');
                 canvas.toBlob(blob => {
-                    uploadIcon(blob);
-                }, 'image/png');
+                    dispatch({
+                        iconFile: blob,
+                        iconSrc: transformedImage
+                    });
+                    callback(blob);
+                });
             });
         }
     };
@@ -132,72 +138,63 @@ function optimizeImage(iconFile, uploadIcon) {
 
 // iPhoneで撮った写真はブラウザ上で回転してしまう。
 // exifに応じて画像の変換(上下左右反転と回転）
-function alignImageOrient(image,orientation) {
-    let canvas = document.createElement('canvas');
-    let width = image.width;
-    let height = image.height;
-    canvas.width = width;
-    canvas.height = height;
-    let ctx = canvas.getContext('2d');
+/*
+function alignImageOrient(iconFile,ctx){
+    orientation = iconFile.exifdata.Orientation;
 
     switch (orientation) {
         case 2:
-            ctx.transform(-1, 0, 0, 1, width, 0);
+            ctx.transform(-1, 0, 0, 1, canvas_width, 0);
             break;
 
         case 3:
-            ctx.transform(-1, 0, 0, -1, width, height);
+            ctx.transform(-1, 0, 0, -1, canvas_width, canvas_height);
             break;
 
         case 4:
-            ctx.transform(1, 0, 0, -1, 0, height);
+            ctx.transform(1, 0, 0, -1, 0, canvas_height);
             break;
 
         case 5:
             ctx.transform(-1, 0, 0, 1, 0, 0);
+            ctx.rotate((90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
             break;
 
         case 6:
-            ctx.transform(1, 0, 0, 1, width, 0);
+            ctx.transform(1, 0, 0, 1, canvas_width, 0);
+            ctx.rotate((90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
             break;
 
         case 7:
-            ctx.transform(-1, 0, 0, 1, width, height);
+            ctx.transform(-1, 0, 0, 1, canvas_width, canvas_height);
+            ctx.rotate((-90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
             break;
 
         case 8:
-            ctx.transform(1, 0, 0, 1, 0, height);
+            ctx.transform(1, 0, 0, 1, 0, canvas_height);
+            ctx.rotate((-90 * Math.PI) / 180);
+            draw_width = canvas_height;
+            draw_height = canvas_width;
             break;
 
         default:
             break;
     }
 
-    let drawWidth, drawHeight;
     if (orientation >= 5) {
         ctx.rotate((-90 * Math.PI) / 180);
-        drawWidth = height;
-        drawHeight = width;
+        draw_width = canvas_height;
+        draw_height = canvas_width;
     }
-    else {
-        drawWidth = width;
-        drawHeight = height;
-    }
-    ctx.drawImage(image, 0, 0, drawWidth, drawHeight);
-    return canvas.toDataURL();
 }
+*/
 
-function resizeImage(image, maxWidth) {
-    let canvas = document.createElement('canvas');
-    let width = image.width;
-    let height = image.height;
+function resizeImage(){
 
-    let scale = maxWidth / width.width;
-    const dstWidth = maxWidth;
-    const dstHeight = height * scale;
-    canvas.width = dstWidth;
-    canvas.height = dstHeight;
-    let ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0, dstWidth, dstHeight);
-    return canvas.toDataURL('image/png');
 }
