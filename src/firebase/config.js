a78ea63c42e470fc1fb0c9732ab9898a599713f4
TetsuFe <tetsubeg1ner27@gmail.com>
2018-11-01 19:09:23 +0900

    Make it enable to switch firebase server on local

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,21 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAPqTg9vMyEenl5t7XR9pdakgaUqa4WdOY",
-  authDomain: "way.satudora.co",
-  databaseURL: "https://whoareyou-c231f.firebaseio.com",
-  projectId: "whoareyou-c231f",
-  storageBucket: "whoareyou-c231f.appspot.com",
-  messagingSenderId: "43340297718"
-};
+export const firebaseConfig = (() => {
+  if (process.env.REACT_APP_ENV === 'development') {
+    return {
+      apiKey: "AIzaSyCBXWCzAv-oAT-HlUgNdHHIgh7fl2O5uDE",
+      authDomain: "whoareyou-dev.firebaseapp.com",
+      databaseURL: "https://whoareyou-dev.firebaseio.com",
+      projectId: "whoareyou-dev",
+      storageBucket: "whoareyou-dev.appspot.com",
+      messagingSenderId: "944348152216"
+    };
+  } else if (process.env.REACT_APP_ENV === 'production') {
+    return {
+      apiKey: "AIzaSyAPqTg9vMyEenl5t7XR9pdakgaUqa4WdOY",
+      authDomain: "way.satudora.co",
+      databaseURL: "https://whoareyou-c231f.firebaseio.com",
+      projectId: "whoareyou-c231f",
+      storageBucket: "whoareyou-c231f.appspot.com",
+      messagingSenderId: "43340297718"
+    };
+  }
+})();
