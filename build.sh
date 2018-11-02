811316fc96a26fc0d522ffadcfa3ae184a42caf2
TetsuFe <tetsubeg1ner27@gmail.com>
2018-11-02 16:43:52 +0900

    Change of deploy process: master to production firebase, others to QA firebase

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'develop' ]]; then
-	npm run-script build:development
-elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
 	npm run-script build:production
 else
 	npm run-script build:development
 fi
+
--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'develop' ]]; then
-	firebase use development --token $FIREBASE_TOKEN
-	firebase deploy --token $FIREBASE_TOKEN
-elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
 	firebase use production --token $FIREBASE_TOKEN
 	firebase deploy --token $FIREBASE_TOKEN
 else
