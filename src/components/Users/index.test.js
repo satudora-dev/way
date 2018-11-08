918ee6181f49fb55fcaea3ec0be4525a408df612
KoheiAsano <komerumeru@i.softbank.jp>
2018-11-08 21:07:22 +0900

    Create Users snapshot

---
+++ src/components/Users/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Users from '../Users';
+import renderer from 'react-test-renderer';
+
+test('Users snapshot', () => {
+  const component = renderer.create(
+    <Users />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
