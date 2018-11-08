7541f07c77870d23540c074349364e58be97e467
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-08 16:00:01 +0900

    Add exif rotation method

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
+import EXIF from 'exif-js';
+
+const createObjectURL
+  = (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
 
 class ImageUploader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      iconSrc: props.iconSrc,
+      rotation: 'rotate(0deg)',
+      transform: 'scale(1,1)',
+    };
+  }
 
-render() {
+  componentWillReceiveProps(nextProps) {
+    this.setState({ iconSrc: nextProps.iconSrc });
+  }
+
+  optimizeImage(e) {
+    let imageFile = e.target.files[0];
+    this.setState({ iconSrc: createObjectURL(imageFile) });
+    EXIF.getData(imageFile, () => {
+      let orientation = imageFile.exifdata.Orientation;
+      let rotation = 0;
+      switch ((orientation - 1) / 2) {
+        case 0:
+          rotation = 'rotate(0deg)';
+        case 1:
+          rotation = 'rotate(180deg)';
+          break;
+        case 2:
+          rotation = 'rotate(90deg)';
+          break;
+        case 3:
+          rotation = 'rotate(270deg)';
+          break;
+        default:
+          break;
+      }
+      let transform = 'scale(1,1)';
+      if (orientation === 2 || orientation === 4
+        || orientation === 5 || orientation === 7) {
+        transform = 'scale(-1,1)';
+      }
+      this.setState({
+        iconSrc: createObjectURL(imageFile),
+        rotation: rotation,
+        transform: transform,
+      })
+    });
+  }
+
+  render() {
 
     const style = {
       imgstyle: {
@@ -13,33 +63,34 @@ render() {
         borderRadius: "50%",
         marginTop: "30px",
         objectFit: "cover",
+        transform: this.state.transform + " " + this.state.rotation,
       },
       btnstyle: {
-        position:"absolute",
+        position: "absolute",
         backgroundColor: "#04B486",
         "color": "white",
-        top:"240px",
-        right:"32px",
-        margin:0,
+        top: "240px",
+        right: "32px",
+        margin: 0,
       },
     }
 
     return (
       <div>
-      <div style={{position:"relative",width:"256px",margin:"auto"}}>
-        <img src={this.props.iconSrc} style={style.imgstyle} alt="Loading..."/>
-        <input type="file" style={{display: "none"}} onChange={e => {
-              this.props.uploadIcon(e.target.files[0],this.props.profileUserKey);
-        }} ref="fileInput"/>
+        <div style={{ position: "relative", width: "256px", margin: "auto" }}>
+          <img src={this.state.iconSrc} style={style.imgstyle} alt="Loading..." />
+          <input type="file" style={{ display: "none" }}
+            onChange={e => this.optimizeImage(e)}
+            ref="fileInput" />
 
-        {(() => {
-          if(this.props.canEdit)
-            return(
-              <Button mini onClick={()=>this.refs.fileInput.click()} variant="fab" style={style.btnstyle}>
-                <EditIcon/>
-              </Button>
-            )
-        })()}
+          {(() => {
+            if (this.props.canEdit)
+              return (
+                <Button mini onClick={() => this.refs.fileInput.click()} variant="fab" style={style.btnstyle}>
+                  <EditIcon />
+                </Button>
+              )
+          })()}
         </div>
         <div className="Login">
         </div>
