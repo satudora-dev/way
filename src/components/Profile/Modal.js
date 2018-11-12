2cb2f9e745d3b8c6ab80ac33e2509aae2f9e9291
yasunari89 <yasunari89@gmail.com>
2018-11-12 15:16:29 +0900

    Create a modal template, Modal.js.

---
+++ src/components/Profile/Modal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import aitl_positions from '../../components/aitl_positions';
+import aitl_projects from '../../components/aitl_projects';
+
+const OriginalModal = ({
+  children,
+  mode,
+  buttonText,
+  modalOpen,
+  currentChecks,
+  updateData,
+  onModalClose,
+  profileUserKey
+}) => {
+  return (
+    <div>
+      <Grid>
+        <Modal
+          open = {modalOpen}
+          onClose = {() => onModalClose()}
+        >
+          <div>
+            {children}
+          </div>
+
+          <Button onClick = {() => onModalClose()}>
+            {buttonText}
+          </Button>
+        <Modal/>
+      </Grid>
+    <div/>
+  )
+}
+
+export default OriginalModal
