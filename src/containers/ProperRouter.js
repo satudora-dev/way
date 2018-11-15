ee68cef9939e237d2e21085d1fa1b36c57e7caf6
KoheiAsano <komerumeru@i.softbank.jp>
2018-11-15 10:57:46 +0900

    Configure Router, Let logout actoin be button

--- src/containers/ProperRouter.js
+++ src/containers/ProperRouter.js
@@ -6,33 +6,15 @@ import * as actions from '../actions';
 class ProperRouter extends React.Component {
 
   render() {
-    if(this.props.match.params.currentPath !== 'users'){
+    const currentPath = this.props.match.params.currentPath;
+    if( (currentPath !== 'users' && currentPath !== 'logout') || currentPath === undefined ){
       this.props.history.push("/users");
     }
-    return null;
-  }
-}
-
-const mapStateToProps = ( {auth, users}, ownProps ) => {
-  const currentUserID = auth.currentUserID;
-  if(!users.init){
-    return {
-      currentUserID: currentUserID,
-      hasOwnProfile: users[currentUserID] !== undefined,
-      currentPath: ownProps.match.params.currentPath
-    }
-  }
-  else{
-    return {
-      currentUserID: currentUserID,
-      hasOwnProfile: true,
-      currentPath: ownProps.match.params.currentPath
-    }
+    return <button onClick={() => console.log(currentPath)}>ASAKO</button>;
   }
 }
 
 
-export default connect(
-  mapStateToProps,
-  null
-)(ProperRouter);
+
+
+export default connect()(ProperRouter);
