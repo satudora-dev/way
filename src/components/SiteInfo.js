53467e00bbba6f42d9931ff0c5a2bfc8b848d630
kitamuyu <33745987+kitamuyu@users.noreply.github.com>
2018-11-21 21:36:01 +0900

    Change styles

--- src/components/SiteInfo.js
+++ src/components/SiteInfo.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import Lottie from 'lottie-react-web';
 import logo from './logo_animation.json';
+import styled from 'styled-components';
+
+
+const SiteGroup = styled.div `
+  padding-top: 30px;
+  text-align: center;
+`
+
 
 
 class SiteInfo extends Component {
   render() {
-    const style = {
-      siteInfo: {
-        "padding-top": "30px",
-        "text-align": "center",
-      },
-      wayLogo: {
-        "vertical-align": "middle",
-        width: 256,
-        height: 128,
-      },
-    }
 
     return (
-      <div style={style.siteInfo}>
-        <Lottie options={{ animationData : logo }}/>
-        <img src="./way.png" style={style.wayLogo}
-             alt="loading the way image..." />
+      <SiteGroup>
+            <Lottie 
+              options={{ animationData : logo }}
+              width={500}
+              height={500}
+            />
         <h2>AI TOKYO LAB & Co.</h2>
-      </div>
+      </SiteGroup>
     );
   }
 }
