b3e8ada6b21bbcf24ff32ee97a10b32b75e2f5d0
TetsuFe <tetsubeg1ner27@gmail.com>
2018-12-02 20:13:45 +0900

    Change layout of project list page to use materialUI Card

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
 
 
 class Projects extends Component {
@@ -14,25 +20,43 @@ class Projects extends Component {
 
   render() {
     const style = {
+      cardWrapper: {
+        width: "90%",
+        margin: "0 auto",
+      },
+      cardButton: {
+        width: "100%",
+        height: "100%"
+      }
     }
     const projects = this.props.projects;
 
     return (
       <div className="Projects">
-        {(()=>{
-          if(projects) {
-            return (Object.keys(projects).map((key, i) => {
-              return (
-                <div>
-                  <a key={i}
-                     onClick={() => this.toProject(key)}>
-                    { projects[key].name }
-                  </a>
-                </div>
-              );
-            }));
-          }
-        })()}
+        <Grid container spacing={24} style={style.cardWrapper}>
+          {(()=>{
+            if(projects) {
+              return (Object.keys(projects).map((key, i) => {
+                return (
+                  <Grid item xs={12} sm={6}>
+                    <Card>
+                      <CardContent>
+                        { projects[key].name }
+                      </CardContent>
+                      <CardActions>
+                        <Button key={i}
+                                onClick={() => this.toProject(key)}
+                                style={style.cardButton}>
+                          詳細
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                );
+              }));
+            }
+          })()}
+        </Grid>
       </div>
     );
   }
