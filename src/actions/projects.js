0231373dfd7cc1faba06fb9107c2ea045e7217dd
TetsuFe <tetsubeg1ner27@gmail.com>
2018-12-02 15:41:13 +0900

    Add projects actions

---
+++ src/actions/projects.js
@@ -0,0 +1,44 @@
+import { fireStore } from '../firebase';
+const projectRef = fireStore.collection('projects');
+
+export const updateProject = (projectID, updatingParams) => dispatch => {
+  projectRef.child(projectID).update(updatingParams)
+    .catch(error => dispatch({
+      type: 'UPDATE_PROJECTS_ERROR',
+      message: error.message,
+    }));
+}
+
+const fetchProjectsSuccess = projects => {
+  return {
+    type: 'RECEIVE_PROJECTS_DATA',
+    data: projects
+  }
+}
+
+const fetchProjects = () => dispatch => {
+  projectRef.onSnapshot((snapshot) => {
+    let projects={};
+    snapshot.docs.forEach((doc) => {
+      projects[doc.id]=doc.data();
+    })
+    dispatch(fetchProjectsSuccess(projects));
+  })
+}
+
+const shouldFetchProjects = (state) => {
+  const projects = state.projects;
+  if (!projects) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const fetchProjectsIfNeeded = () => (dispatch, getState) => {
+  if (!shouldFetchProjects(getState())) {
+    return dispatch(fetchProjects());
+  }
+}
+
+
