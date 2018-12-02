dd90d215e6b028fb8b5f8d9c73b68f2a4ae812f6
TetsuFe <tetsubeg1ner27@gmail.com>
2018-12-02 15:43:13 +0900

    Modify projects reducer to fit firestore

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -1,21 +1,9 @@
 const projects = (state = {noData: true}, action) => {
   switch (action.type) {
     case 'RECEIVE_PROJECTS_DATA':
-      let projects ={}
       if(action.data){
-        Object.keys(action.data).forEach(key =>{
-          let members = []
-          Object.keys(action.data[key].members).forEach(userkey =>{
-            members.push({userkey})
-          });
-          projects[key]={
-            members: members,
-            name: action.data[key].name,
-            description: action.data[key].description,
-          }
-        });
+        return action.data
       }
-      return projects
     case 'RECEIVE_PROJECTS_ERROR':
     case 'UPDATE_PROJECTS_ERROR':
       // alert(action.message)
