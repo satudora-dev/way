4605bd087f3f83d27077b4731317d42eb8d04df5
TetsuFe <tetsubeg1ner27@gmail.com>
2018-12-04 14:11:30 +0900

    Change project list component from class to functional

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -6,61 +6,52 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 
+const Projects = ({ projects, history }) => {
 
-class Projects extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-    };
+  const toProject = id => {
+    history.push(`/projects/${id}`);
   }
 
-  toProject(id){
-    this.props.history.push(`/projects/${id}`);
-  }
-
-  render() {
-    const style = {
-      cardWrapper: {
-        width: "90%",
-        margin: "0 auto",
-      },
-      cardButton: {
-        width: "100%",
-        height: "100%"
-      }
+  const style = {
+    cardWrapper: {
+      width: "90%",
+      margin: "0 auto",
+    },
+    cardButton: {
+      width: "100%",
+      height: "100%"
     }
-    const projects = this.props.projects;
+  };
 
-    return (
-      <div className="Projects">
-        <h2>プロジェクト一覧</h2>
-        <Grid container spacing={24} style={style.cardWrapper}>
-          {(()=>{
-            if(projects) {
-              return (Object.keys(projects).map((key, i) => {
-                return (
-                  <Grid item xs={12} sm={6}>
-                    <Card>
-                      <CardContent>
-                        { projects[key].name }
-                      </CardContent>
-                      <CardActions>
-                        <Button key={i}
-                                onClick={() => this.toProject(key)}
-                                style={style.cardButton}>
-                          詳細
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                );
-              }));
-            }
-          })()}
-        </Grid>
-      </div>
-    );
-  }
+  return (
+    <div className="Projects">
+      <h2>プロジェクト一覧</h2>
+      <Grid container spacing={24} style={style.cardWrapper}>
+        {(()=>{
+          if(projects) {
+            return (Object.keys(projects).map((key, i) => {
+              return (
+                <Grid item xs={12} sm={6}>
+                  <Card>
+                    <CardContent>
+                      { projects[key].name }
+                    </CardContent>
+                    <CardActions>
+                      <Button key={i}
+                              onClick={() => toProject(key)}
+                              style={style.cardButton}>
+                        詳細
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            }));
+          }
+        })()}
+      </Grid>
+    </div>
+  );
 }
 
 
