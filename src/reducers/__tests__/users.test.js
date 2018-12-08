1d3762ca46875c87244bd907c134a15c5061f902
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-08 13:03:11 +0900

    Remove unused test

--- src/reducers/__tests__/users.test.js
+++ src/reducers/__tests__/users.test.js
@@ -31,48 +31,8 @@ describe('users reducer', () => {
         icon: "https:firebaseapp."
       }
     }
-    const inputValue1 = {
-      '-LMfORGHgxYDl25ca6V2': {
-        family: "asano",
-        given: "kohei",
-        position: "アルバイト",
-        projects: ["アプリ開発","コーポレートスタッフ"],
-        tags: ["TRAVIS","尊師"],
-        icon: "https:firebaseapp."
-      },
-      '-LMfORGHgxYDl25ca6V2': {
-        family: "asano",
-        given: "kohei",
-        position: "アルバイト",
-        projects: ["アプリ開発","コーポレートスタッフ"],
-        tags: ["TRAVIS","尊師"],
-        icon: "https:firebaseapp."
-      }
-    }
-    const expectedState1={
-      '-LMfORGHgxYDl25ca6V2': {
-        family: "asano",
-        given: "kohei",
-        position: "アルバイト",
-        projects: ["アプリ開発","コーポレートスタッフ"],
-        tags: ["TRAVIS","尊師"],
-        icon: "https:firebaseapp."
-      },
-      '-LMfORGHgxYDl25ca6V2': {
-        family: "asano",
-        given: "kohei",
-        position: "アルバイト",
-        projects: ["アプリ開発","コーポレートスタッフ"],
-        tags: ["TRAVIS","尊師"],
-        icon: "https:firebaseapp."
-      }
-    }
     expect(
       usersReducer([],{type: 'RECEIVE_USERS_DATA',data: inputValue0})
     ).toEqual(expectedState0)
-
-    expect(
-      usersReducer([],{type: 'RECEIVE_USERS_DATA',data: inputValue1})
-    ).toEqual(expectedState1)
   })
 })
