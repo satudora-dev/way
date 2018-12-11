773dbcbf071c50c4ef61108afe62bcacad64cd9e
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-11 15:19:43 +0900

    Remove unused Import

--- src/components/Project/ProjectList.js
+++ src/components/Project/ProjectList.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
