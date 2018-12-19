69476fa3f5fc7238d27ac5aca0e3d662baa62bd9
yasunari89 <yasunari89@gmail.com>
2018-12-19 14:11:48 +0900

    delete Checkbox in src/components/Users/NowGroupFilter

--- src/components/Users/NowGroupFilter.js
+++ src/components/Users/NowGroupFilter.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-
 import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
-
 import nowGroupList from '../nowGroupList';
+
 
 const style = {
   tagStyle: {
