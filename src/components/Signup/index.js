57e10c050d70b22f1d03bafbd661df52aa2191d5
yasunari89 <yasunari89@gmail.com>
2018-12-19 14:16:23 +0900

    delete import EXIF in src/Signup/index

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import EXIF from 'exif-js';
 import SiteInfo from "../../components/SiteInfo";
 import IconPreview from '../IconPreview';
 
