48af12270a9a7db1e1ff7b416578108f6c3f1756
yasunari89 <yasunari89@gmail.com>
2019-01-08 20:44:04 +0900

    Create repo_page.js

---
+++ src/components/repo_page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class RepoPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Test Code.
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default RepoPage
