95c6a70276fc56f9abc936ecdd5607f41faf25be
yasunari89 <yasunari89@gmail.com>
2019-01-08 21:09:29 +0900

    install line chart

--- src/components/repo_page.js
+++ src/components/repo_page.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { LineChart, Line } from 'recharts'
+
+
+const data = [
+  {name: 'Page A', uv: 4000},
+  {name: 'Page B', uv: 3000},
+  {name: 'Page C', uv: 2000},
+  {name: 'Page D', uv: 2780},
+  {name: 'Page E', uv: 1890},
+  {name: 'Page F', uv: 2390},
+  {name: 'Page G', uv: 3490},
+]
 
 class RepoPage extends React.Component {
+
   render () {
     return (
       <div>
-        <h1>
-          Test Code.
-        </h1>
+        <LineChart width={400} height={400} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        </LineChart>
       </div>
     )
   }
