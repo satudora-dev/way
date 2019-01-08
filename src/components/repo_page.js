8c5b28780dec3a94b86cffa9726915dbc5619398
yasunari89 <yasunari89@gmail.com>
2019-01-08 21:48:17 +0900

    create barchart

--- src/components/repo_page.js
+++ src/components/repo_page.js
@@ -1,25 +1,36 @@
 import React from 'react'
-import { LineChart, Line } from 'recharts'
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts'
 
 
 const data = [
-  {name: 'Page A', uv: 4000},
-  {name: 'Page B', uv: 3000},
-  {name: 'Page C', uv: 2000},
-  {name: 'Page D', uv: 2780},
-  {name: 'Page E', uv: 1890},
-  {name: 'Page F', uv: 2390},
-  {name: 'Page G', uv: 3490},
+  {name: 'Monday', Active: 400, Merged: 180, Closed: 130},
+  {name: 'Tuesday', Active: 300, Merged: 90, Closed: 320},
+  {name: 'Wednesday', Active: 200, Merged: 10, Closed: 130},
+  {name: 'Thursday', Active: 270, Merged: 190, Closed: 340},
+  {name: 'Friday', Active: 180, Merged: 180, Closed: 130},
+  {name: 'Saturday', Active: 230, Merged: 890, Closed: 30},
+  {name: 'Sunday', Active: 340, Merged: 190, Closed: 230},
 ]
+
+const styleBeta = {
+}
 
 class RepoPage extends React.Component {
 
   render () {
     return (
       <div>
-        <LineChart width={400} height={400} data={data}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        </LineChart>
+        Satoshi Yoshio
+        <BarChart style={styleBeta} width={700} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* <XAxis dataKey="name" /> */}
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Active" fill="#94FC9F" stackId="a"/>
+          <Bar dataKey="Merged" fill="#EBFC94" stackId="a"/>
+          <Bar dataKey="Closed" fill="#FCCE94" stackId="a"/>
+        </BarChart>
       </div>
     )
   }
