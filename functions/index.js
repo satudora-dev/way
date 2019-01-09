12070e822e8504add8883c7913716106e3fbebed
TetsuFe <tetsubeg1ner27@gmail.com>
2019-01-09 14:01:48 +0900

    Add cloud function to fetch github user profile

--- functions/index.js
+++ functions/index.js
@@ -59,3 +59,6 @@ function sendNewComerEmail(email, newUserFullName, newUserID) {
     console.log(error.message);
   });
 }
+
+
+exports.fetchRepositoryUserProfiles = require('./fetchRepositoryUserProfiles');
