e96f1ad4b52bf83eb75fc70848106d0cbe2610c4
TetsuFe <tetsubeg1ner27@gmail.com>
2019-02-01 19:40:21 +0900

    Update get commit log funciton(complete to use firebase function)

--- functions/githubReport/getCommitFromGitLog.js
+++ functions/githubReport/getCommitFromGitLog.js
@@ -2,9 +2,40 @@
 
 const functions = require('firebase-functions');
 
-function showGitLog(){
-  const clone = require('simple-git')().clone('https://github.com/TetsuFe/my-official-site.git', './');
-  console.log(clone.log());
+/* TODD: repoNameのところを任意にする */
+async function fetchRepositoryIfNeededAndShowLog(){
+  const fs = require('fs');
+  const path = './repoName';
+
+  try {
+    fs.statSync(path);
+    await showGitLog(path);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await require('simple-git')().clone('https://github.com/TetsuFe/my-official-site.git', './repoName');
+      await showGitLog(path);
+    } else {
+      console.log('予期しないエラーが発生しました。');
+    }
+  }
+}
+
+
+async function showGitLog(path){
+  const git = await require('simple-git')
+  git(path).raw(
+    [
+      'log',
+      '--no-merges',
+      '--date=short',
+      '--pretty=%h %cd %an %d %s'
+    ], (err, result) => {
+      console.log(result)
+    });
+  /* 以下の書き方にするとundefinedになる
+  const log = await require('simple-git')('./repoName').log();
+  console.log(log);
+  */
 }
 
 
@@ -16,3 +47,7 @@ module.exports = functions.https.onRequest((req, res) => {
     res.status(200).end('fail: require GET request to call github api')
   }
 })
+
+
+/* test */
+//fetchRepositoryIfNeededAndShowLog();
